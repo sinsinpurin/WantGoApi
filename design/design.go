033f38c8294a11307fe363a2da928d63589b8b76
sinsinpurin <tusanch6@gmail.com>
2020-03-16 21:42:35 +0900

    no message

--- design/design.go
+++ design/design.go
@@ -37,6 +37,12 @@ var _ = API("WantGoApi", func() {
 		cors.Methods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 		cors.MaxAge(600)
 	})
+
+	cors.Origin("https://wantgo-facf0.web.app/card/2", func() {
+		cors.Headers("X-Requested-With", "Content-Type", "Accept", "Origin")
+		cors.Methods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+		cors.MaxAge(600)
+	})
 })
 
 var simpleCard = Type("simpleCard", func() {
