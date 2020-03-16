0cd0485323472c23c49a3fb5cd39524374fe8e9f
sinsinpurin <tusanch6@gmail.com>
2020-03-16 14:29:26 +0900

    no message

--- design/design.go
+++ design/design.go
@@ -27,7 +27,7 @@ var _ = API("WantGoApi", func() {
 	})
 
 	cors.Origin("https://wantgo-facf0.firebaseapp.com", func() {
-		cors.Headers("X-Requested-With", "Content-Type", "X-Token-Auth", "Authorization", "application/json", "Origin", "Accept", "text/plain")
+		cors.Headers("X-Requested-With", "Content-Type", "application/json", "Accept", "text/plain")
 		cors.Methods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 		cors.Expose("X-Time")
 		//cors.MaxAge(600)
