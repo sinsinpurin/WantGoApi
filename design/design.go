957be5604fdcdce139b99bcd76e959ff4d977438
sinsinpurin <tusanch6@gmail.com>
2020-03-16 21:55:39 +0900

    no message

--- design/design.go
+++ design/design.go
@@ -2,7 +2,6 @@ package design
 
 import (
 	. "goa.design/goa/v3/dsl"
-	cors "goa.design/plugins/v3/cors/dsl"
 )
 
 var _ = API("WantGoApi", func() {
@@ -38,11 +37,11 @@ var _ = API("WantGoApi", func() {
 	// 	cors.MaxAge(600)
 	// })
 
-	cors.Origin("*", func() {
-		cors.Headers("X-Requested-With", "Content-Type", "Accept", "Origin")
-		cors.Methods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-		cors.MaxAge(600)
-	})
+	// cors.Origin("*", func() {
+	// 	cors.Headers("X-Requested-With", "Content-Type", "Accept", "Origin")
+	// 	cors.Methods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	// 	cors.MaxAge(600)
+	// })
 })
 
 var simpleCard = Type("simpleCard", func() {
